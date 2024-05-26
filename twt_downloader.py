3aca55844aa3e52618fe6ec77c34bf3a24a2cae7
NicholasStout <32071336+NicholasStout@users.noreply.github.com>
2024-05-26 15:09:16 -0500

    clean up code and add gif and large gallery download

--- twt_downloader.py
+++ twt_downloader.py
@@ -2,40 +2,89 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+
 import requests
 import time
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://twitter.com/i/bookmarks")
+POST_NAME='css-175oi2r.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu'
+POST_IMAGE_NAME='css-175oi2r.r-1pi2tsx.r-1ny4l3l.r-1loqt21'
+
+service = webdriver.FirefoxService(executable_path='/home/knil/Projects/WebDrivers/geckodriver')
+options = webdriver.FirefoxOptions()
+options.add_argument('--start-maximized');
+options.add_argument('--start-fullscreen');
+driver = webdriver.Firefox(service=service, options=options)
+driver.get("https://x.com/i/bookmarks")
 
 input("Sign in, fullscreen, and press enter:")
 
-while(True):
-    content = driver.find_elements(By.CLASS_NAME, 'css-175oi2r.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu')
-    content = content[0].find_elements(By.CLASS_NAME, 'css-175oi2r.r-9aw3ui.r-1s2bzr4')
-    imgs = content[0].find_elements(By.TAG_NAME, 'a')
-    link = [i for i in imgs if len(i.find_elements(By.TAG_NAME, 'img')) > 0]
-    link[0].click()
-
-    for i in range(len(link)):
-        img = driver.find_elements(By.TAG_NAME, 'img')
-        img_url = img[i].get_attribute('outerHTML')
-        img_url = img_url.split('src=')[1].split(' class')[0].replace('"', '').replace('amp;','')
-        req = requests.get(img_url)
-        name = img_url.split('?')[0].split('/')[-1]
-        form = img_url.split('format=')[-1].split('&')[0]
-        f = open('temp/'+name+'.'+form, 'wb')
-        f.write(req.content)
-        f.close()
+def img_downloader(webC):
+    img_url = webC.get_attribute('outerHTML')
+    if ('240x240' in img_url) or ('120x120' in img_url):
+        print("too small" + img_url)
+        return
+    img_url = img_url.split('src=')[1].split(' class')[0].replace('"', '').replace('amp;','').replace('360x360','large').replace('small','large').replace('medium','large')
+    print(img_url)
+    req = requests.get(img_url)
+    name = img_url.split('?')[0].split('/')[-1]
+    form = img_url.split('format=')[-1].split('&')[0]
+    f = open('temp/'+name+'.'+form, 'wb')
+    f.write(req.content)
+    f.close()
     
-    if len(link) > 2:
-        input("Large collection detected. Download manually then press enter.")
-    button = driver.find_element(By.CLASS_NAME, 'css-175oi2r.r-1kbdv8c.r-18u37iz.r-1oszu61.r-3qxfft.r-s1qlax.r-2sztyj.r-1efd50x.r-5kkj8d.r-h3s6tt.r-1wtj0ep')
-    buttons = button.find_elements(By.CLASS_NAME, 'css-175oi2r.r-1777fci.r-bt1l66.r-bztko3.r-lrvibr.r-1loqt21.r-1ny4l3l')
-    buttons[3].click()
-    driver.back()
-    time.sleep(1)
+
+def gif_downloader(webC):
+    img_url = webC.get_attribute('outerHTML')
+    img_url = img_url.split('src=')[1].split(' type')[0].replace('"', '')
+    req = requests.get(img_url)
+    name = img_url.split('?')[0].split('/')[-1]
+    f = open('temp/'+name, 'wb')
+    f.write(req.content)
+   
+
+def video_downloader(webC):
+    input("Copy video link, put into twitter downloader of your choice, then press enter.")
+
+            
+
+def isolator(webC):
+    for c in webC:
+        if len(c.find_elements(By.TAG_NAME, 'img')) != 0:
+            img_downloader(c)
+        elif len(c.find_elements(By.TAG_NAME, 'source')) != 0:
+            video_downloader(c)
+        elif len(c.find_elements(By.TAG_NAME, 'video')) != 0:
+            gif_downloader(c)
+        
+
+def click_bookmark(webC):
+    bm = webC.find_element(By.CSS_SELECTOR, "[aria-label='Bookmarked']")
+    bm.click()
+            
+
+
+while(True):
+    try:
+        content1 = driver.find_elements(By.CLASS_NAME, POST_NAME)
+        #content1[0].screenshot('fizz.png')
+        content2 = content1[0].find_elements(By.CLASS_NAME, 'css-175oi2r.r-1pi2tsx.r-1ny4l3l.r-1loqt21')
+        if len(content2) == 0:
+            content2 = content1[0].find_elements(By.CLASS_NAME, 'css-175oi2r.r-9aw3ui.r-1s2bzr4')
+    
+
+        if len(content2) == 0:
+            video_downloader(content2)
+    #continue
+        else:
+            isolator(content2)
+    
+
+        click_bookmark(content1[0])
+        time.sleep(1)
+        #input("press enter to continue.")
+    
+    except Exception as e:
+        input("An error occured!" +str(e)+ "fix the error then press enter to continue.")
+        continue
     
 
